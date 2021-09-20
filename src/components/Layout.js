54770714b1c62195a9510a2420c2de61453cc3aa
Apple <apple@Apples-iMac.local>
2021-09-20 15:01:36 -0700

    Updated functionality

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,4 +11,5 @@ function Layout(props){
         </>
     )
 }
+
 export default Layout;
--- src/components/MouseParallax.js
+++ src/components/MouseParallax.js
@@ -1,12 +1,67 @@
-import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import React, { useState, useEffect } from "react";
+import * as parallaxStyles from './styles/MouseParallax.module.scss'
 
 function MouseParallax (props){
+    let windowWidth;
+    let windowHeight;
+
+    useEffect(()=>{
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
+
+        const onresize = () => {
+            windowWidth = window.innerWidth;
+            windowHeight = window.innerHeight;
+        }
+        window.addEventListener('onresize', onresize)
+    })
+
+    const [mousePos, setPosition] = useState('50% 50%');
+    const [graphicTop, setTop] = useState('-5%')
+    const [graphicRight, setRight] = useState('-5%')
+    const parallax = (e) => {
+        let posX = Math.floor((e.clientX * 100) / windowWidth)
+        let posY = Math.floor((e.clientY * 100) / windowHeight)
+        
+        setPosition( posX + '% ' + posY + '%')
+
+        let graphicX = ((posX / 100) * 5 ) - 5
+        let graphicY = (posY / 100) * - 5
+        setRight(graphicX + '%')
+        setTop(graphicY + '%')
+        //console.log(graphicX)
+    }
+
+    let graphic;
+    let background;
+    if( props.page === 'home'){
+        graphic = <StaticImage
+                    src="../images/AMX_Web-Home-01-BannerPrincipal-Grafico.png"
+                    alt=""
+                    placeholder="none"
+                    className={ parallaxStyles.graphic }
+                    style={ { top: graphicTop, right: graphicRight } }
+                  />
+        background = parallaxStyles.home
+    }
+    else if( props.page === "about" ){
+        background = parallaxStyles.about
+    }
+    else if( props.page === "services" ){
+        background = parallaxStyles.services
+    }
 
     return (
-        <section>
-            { props.children }
+        <section className={ parallaxStyles.container + ' ' + background } style={ { backgroundPosition: mousePos } } onMouseMove={ parallax } role='banner'>
+            <div className="row">
+                { graphic }
+                <div className="single">
+                    { props.children }
+                </div>
+            </div>
         </section>
     )
-
+    
 }
 export default MouseParallax;
